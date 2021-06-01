6d0b8de6ba54f7b71f1e13598f108fccc4e1c3bc
Rodion <Radium-ON@outlook.com>
2021-06-01 19:26:34 +0700

    Parameter type dto

---
+++ SpeechTrainer.Database/Entities/ParameterTypeDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeechTrainer.Database.Entities
+{
+    public class ParameterTypeDto
+    {
+        public int Id { get; private set; }
+        public string TypeName { get; private set; }
+
+        public ParameterTypeDto()
+        {
+            
+        }
+
+        public ParameterTypeDto(int id, string typeName)
+        {
+            Id = id;
+            TypeName = typeName;
+        }
+    }
+}
