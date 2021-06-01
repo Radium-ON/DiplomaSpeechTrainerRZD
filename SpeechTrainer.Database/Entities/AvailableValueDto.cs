36d33f40931171a98503145f44f9fb604b7558aa
Rodion <Radium-ON@outlook.com>
2021-06-01 19:33:26 +0700

    Доступные значения (таблица) dto

---
+++ SpeechTrainer.Database/Entities/AvailableValueDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeechTrainer.Database.Entities
+{
+    public class AvailableValueDto
+    {
+        public int Id { get; private set; }
+        public ParameterTypeDto ParameterType{ get; private set; }
+        public string Value { get; private set; }
+
+        public AvailableValueDto(int id, ParameterTypeDto parameterType, string value)
+        {
+            Id = id;
+            ParameterType = parameterType;
+            Value = value;
+        }
+
+        public void SetParameterType(ParameterTypeDto parameterType)
+        {
+            ParameterType = parameterType;
+        }
+    }
+}
