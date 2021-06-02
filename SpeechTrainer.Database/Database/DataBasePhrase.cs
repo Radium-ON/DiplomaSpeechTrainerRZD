0e0f17a835e344260b13282c2a5b1e321e14b1ca
Rodion <Radium-ON@outlook.com>
2021-06-02 22:04:18 +0700

    Запросы select для Phrase

--- SpeechTrainer.Database/Database/DataBasePhrase.cs
+++ SpeechTrainer.Database/Database/DataBasePhrase.cs
@@ -16,5 +16,87 @@ namespace SpeechTrainer.Database.Database
         {
             _client = DatabaseConnection.Source;
         }
+
+        #region Implementation of IDatabase<PhraseDto,bool>
+
+        public async Task<List<PhraseDto>> SelectAllAsync()
+        {
+            var command = "SELECT * FROM Phrase";
+            var phrases = new List<PhraseDto>();
+            try
+            {
+                using (var cmd = new SqlCommand(command, _client.OpenConnection()))
+                {
+                    var dataReader = await cmd.ExecuteReaderAsync();
+                    while (dataReader.Read())
+                    {
+                        var id = dataReader.GetInt32(0);
+                        var text = dataReader.GetString(1);
+
+                        var phrase = new PhraseDto(id, text);
+                        phrases.Add(phrase);
+                    }
+                }
+
+                _client.CloseConnection();
+                return phrases;
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine("[DatabasePhrase.SelectAllAsync()] Error: " + exception.Message);
+                _client.CloseConnection();
+                return null;
+            }
+            finally
+            {
+                _client.CloseConnection();
+            }
+        }
+
+        public async Task<PhraseDto> SelectByIdAsync(int idObject)
+        {
+            var command = "SELECT * FROM Phrase WHERE Id = @ID";
+            var phraseDto = new PhraseDto();
+            try
+            {
+                using (var cmd = new SqlCommand(command, _client.OpenConnection()))
+                {
+                    cmd.Parameters.AddWithValue("@ID", idObject);
+                    var dataReader = await cmd.ExecuteReaderAsync();
+                    while (dataReader.Read())
+                    {
+                        var id = dataReader.GetInt32(0);
+                        var text = dataReader.GetString(1);
+
+                        phraseDto = new PhraseDto(id, text);
+                    }
+                }
+
+                _client.CloseConnection();
+                return phraseDto;
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine("[DatabasePhrase.SelectByIdAsync()] Error: " + exception.Message);
+                _client.CloseConnection();
+                return null;
+            }
+            finally
+            {
+                _client.CloseConnection();
+            }
+        }
+
+        public async Task<bool> UpdateAsync(PhraseDto newObject)
+        {
+            return false;//todo
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return false;//todo
+        }
+
+        #endregion
     }
 }
